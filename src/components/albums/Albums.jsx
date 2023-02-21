73687811a4d6b8d655fcf821630fbf5cad8e7507
Mohakim231 <mohamedhakim0381@gmail.com>
2023-02-21 08:24:58 +0000

    adding fix

---
+++ src/components/albums/Albums.jsx
@@ -0,0 +1,17 @@
+export const Albums = [
+    {
+        title : "Nancy 10",
+        date: "2021",
+        image: "https://th.bing.com/th/id/OIP.uoN8hRe660Gd8Le0F2pSpQHaHa?w=183&h=183&c=7&r=0&o=5&pid=1.7"
+    },
+    {
+        title : "Nancy 9 Hassa Beek",
+        date: "2017",
+        image: "https://th.bing.com/th/id/OIP.QGjAnR-7DWv9HtdEBJoiDgHaGi?w=208&h=183&c=7&r=0&o=5&pid=1.7"
+    },
+    {
+        title : "Nancy 8",
+        date: "2014",
+        image: "https://th.bing.com/th/id/OIP.F20ccPrcMO768t_sw6uQAwHaGc?w=195&h=180&c=7&r=0&o=5&pid=1.7" 
+    }
+]
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,25 +1,20 @@
-import React from 'react'
-import './List.css'
+import React from 'react';
+import './List.css';
+import { Albums } from '../albums/albums';
 
 function List() {
   return (
     <div className='list'>
     <ul className='albumList'>
-        <li>
-          <h3>Album title</h3>
-          <p>release date:00/00/0000</p>
-          <img src="https://th.bing.com/th/id/OIP.uoN8hRe660Gd8Le0F2pSpQHaHa?w=183&h=183&c=7&r=0&o=5&pid=1.7"/>
+      {Albums.map((a, index) => {
+        return(
+          <li key={index}>
+          <h3>{a.title}</h3>
+          <p>{a.date}</p>
+          <img src={a.image}/>
         </li>
-        <li>
-          <h3>Album title</h3>
-          <p>release date:00/00/0000</p>
-          <img src="https://th.bing.com/th/id/OIP.QGjAnR-7DWv9HtdEBJoiDgHaGi?w=208&h=183&c=7&r=0&o=5&pid=1.7"/>
-        </li>
-        <li>
-          <h3>Album title</h3>
-          <p>release date:00/00/0000</p>
-          <img src="https://th.bing.com/th/id/OIP.F20ccPrcMO768t_sw6uQAwHaGc?w=195&h=180&c=7&r=0&o=5&pid=1.7"/>
-        </li>
+        )
+      })} 
     </ul>
     <form>
       <input type="text" name='title' placeholder='Title..'/><br></br>
